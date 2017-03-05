1f2b73e19380a3df4a7dded581ab4b0b5b430618
radekkania <radekkania7@wp.pl>
2017-03-05 21:02:33 +0100

    add 4 test for method min in CalculatorTest

--- calculator/src/test/java/wdsr/exercise1/logic/CalculatorTest.java
+++ calculator/src/test/java/wdsr/exercise1/logic/CalculatorTest.java
@@ -1,5 +1,6 @@
 package wdsr.exercise1.logic;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -59,5 +60,52 @@ public class CalculatorTest {
 		
 		// then
 		// empty
-	}		
+	}	
+	
+	@Test
+	public void testMax_shouldReturnTheBigestOfAllValues() {
+		//given
+		int [] values = { 1, 2, 3 };
+		
+		//when
+		int max = calculator.max(values);
+		
+		//then
+		assertThat(max,is(values[2]));
+	}
+	
+	@Test
+	public void testMax_shouldReturnTheOnlyValue() {
+		//given
+		int [] values = { 5 };
+		
+		//when
+		int max = calculator.max(values);
+		
+		//then
+		assertThat(max,is(values[0]));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testMax_shouldRaiseAnExceptionForEmptyArgument() {
+		//given
+		int [] values = {};
+		
+		//when
+		calculator.max(values);
+		//then
+		//empty
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testMax_shouldRaiseAnExceptionForNullArgument() {
+		//given
+		int [] values = null;
+		
+		//when
+		calculator.max(values);
+		//then
+		//empty
+	}
+
 }
